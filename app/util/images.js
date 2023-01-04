17b7319cd8a616e450280dd18bf43ad2e28b4387
Yilia <1418315068@qq.com>
2023-01-04 14:35:29 +0800

    commit

--- app/util/images.js
+++ app/util/images.js
@@ -6,13 +6,6 @@ const images = {
       "https://img1.baidu.com/it/u=3225128568,427261393&fm=253&fmt=auto&app=138&f=JPEG?w=516&h=500",
     testImg: `https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202004%2F08%2F20200408220627_R8UeF.thumb.1000_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1672656046&t=d28e8154c61c54192644bd02f59403bf`,
   },
-  welcome: {
-    address: require("../static/img/address.png"),
-    list: require("../static/img/list.png"),
-    andr: require("../static/img/andr.png"),
-    ios: require("../static/img/ios.png"),
-    smAndr: require("../static/img/sm_andr.png"),
-    smIos: require("../static/img/sm_ios.png"),
-  },
+  welcome: {},
 };
 export default images;
